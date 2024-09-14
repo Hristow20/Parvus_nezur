76c933207b1ba16c5932d83a7f2850b5e2a3bf20
Hristow20 <92942468+Hristow20@users.noreply.github.com>
2024-09-14 13:25:28 +0300

    Update UI.lua

--- Utilities/UI.lua
+++ Utilities/UI.lua
@@ -3360,9 +3360,9 @@ Bracket.Elements = {
 					local ColorX = math.clamp(Mouse.X - PaletteAsset.SVPicker.AbsolutePosition.X, 0, PaletteAsset.SVPicker.AbsoluteSize.X) / PaletteAsset.SVPicker.AbsoluteSize.X
 					local ColorY = math.clamp(Mouse.Y - (PaletteAsset.SVPicker.AbsolutePosition.Y + GuiInset.Y), 0, PaletteAsset.SVPicker.AbsoluteSize.Y) / PaletteAsset.SVPicker.AbsoluteSize.Y
 
-					--Colorpicker.Value[2] = ColorX
-					--Colorpicker.Value[3] = 1 - ColorY
-					--Colorpicker.Value = Colorpicker.Value
+					Colorpicker.Value[2] = ColorX
+					Colorpicker.Value[3] = 1 - ColorY
+					Colorpicker.Value = Colorpicker.Value
 				end)
 			end
 		end)
@@ -3633,7 +3633,7 @@ Bracket.Elements = {
 	end
 }
 
-Bracket.Elements.Screen()
+--Bracket.Elements.Screen()
 function Bracket:Window(Window)
 	Window = Bracket.Utilities:GetType(Window, {}, "table", true)
 	Window.Blur = Bracket.Utilities:GetType(Window.Blur, false, "boolean")
